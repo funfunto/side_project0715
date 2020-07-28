0f5eac3d33a97d053adbec41b0f0986c78a89a20
funfunto <49137900+funfunto@users.noreply.github.com>
2020-07-29 01:12:33 +0800

    0728

---
+++ javascripts/initFullPage.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
+    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.2)', 'rgba(0,0,0,.2)', 'rgba(0,0,0,.8)', '#222'],
+    scrollOverflow: true,
+    menu: '#header'
+  });
+  $(document).on('click', '#btnGoDown', function(){
+    $.fn.fullpage.moveSectionDown();
+  });
+  $(".expCard").hide();
+  
+  let bubble = [".babyBlue", ".hotPink", ".yellow", ".orange", ".green", ".purple"];
+  let card = [".translate", ".pianoClub", ".shopping", ".government", ".campaign", ".technical"];
+  for(let i=0; i<6; i++) {
+    $(bubble[i]).hover(function() {
+      $(card[i]).fadeIn(60);
+    }, function() {
+      $(card[i]).fadeOut(60);
+    });
+  }
+});

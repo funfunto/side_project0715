31c08eb18698d404b8363f1d70051ab6d9de41f4
funfunto <49137900+funfunto@users.noreply.github.com>
2020-07-30 02:20:38 +0800

    0730

--- js/initFullPage.js
+++ js/initFullPage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
-    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.2)', 'rgba(0,0,0,.2)', 'rgba(0,0,0,.8)', '#222'],
+    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.2)', 'rgba(0,0,0,.2)', 'rgba(217 187 187 / 90%)', '#222'],
     scrollOverflow: true,
     menu: '#header'
   });
